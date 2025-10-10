cf42de4ea8aade51d82d0215815e11b0889d9bca
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2025-10-10 03:12:33 +0300

    up

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -24,8 +24,8 @@ export const CityCard = ({ id, city, data, onDelete, onExpand }) => {
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
         alt={description}
         /> */}
-      <p className={s.time}>{description}</p>
       <p className={s.temp}>{Math.round(data.main.temp)}℃</p>
+      <p className={s.time}>{description}</p>
       <button type="button" className={s.moreBtn} onClick={onExpand}>
         See more
       </button>
